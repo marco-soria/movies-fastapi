d2dc8670d77382b7da33429981d7f69543b7235b
marco-soria <marco.soriaa7@gmail.com>
2024-09-02 22:07:20 -0500

    get

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
+from fastapi.responses import HTTPResponse
 
 app = FastAPI()
+app.title = 'My First FastAPI App'
+app.version = '0.0.1'
 
-@app.get('/')
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    }
+]
+
+@app.get('/', tags=['home'])
 def message():
-    return {'message': 'Hello World'}
+    return HTMLResponse(content='<h1>Welcome to FastAPI</h1>', status_code=200)
+
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
